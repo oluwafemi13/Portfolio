98ef8cf52081b4b44cc6d349b046079a86802a9e
IKECHUKWU <sundayjabikem@gmail.com>
2023-05-23 10:35:40 +0100

    update

--- EmailSettings.cs
+++ EmailSettings.cs
@@ -1,10 +1,9 @@
-﻿namespace Portfolio
-{
-    public class EmailSettings
+﻿namespace Portfolio;
 
-    {
-    public string Subject { get; set; }
-    public string Body{get; set; }
-    public string Email { get; set; }
-    }
+public class EmailSettings{
+
+public string Subject { get; set; }
+public string Body{get; set; }
+public string Email { get; set; }
+public string Name { get; set; }
 }
--- Utility/EmailSender.cs
+++ Utility/EmailSender.cs
@@ -1,14 +1,23 @@
 ﻿using MailKit.Net.Smtp;
 using MimeKit;
 using Portfolio.Utility.Interface;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.ComponentModel.DataAnnotations;
 
 namespace Portfolio.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public EmailSender(IConfiguration config)
+        {
+            SendGridSecret = config.GetValue<string>("SendGrid: SecretKey");
+        }
+
+        private string SendGridSecret { get; set; }
         public Task SendEmailAsync(EmailSettings settings)
         {
-            var emailToSend = new MimeMessage();
+            /*var emailToSend = new MimeMessage();
            
             emailToSend.From.Add(MailboxAddress.Parse(settings.Email));
             emailToSend.To.Add(MailboxAddress.Parse("sundayjabikem@gmail.com"));
@@ -19,7 +28,14 @@ namespace Portfolio.Utility
             using (var emailClient = new SmtpClient())
             {
                 emailClient.Connect()
-            }
+            }*/
+
+            var client = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress(settings.Email, settings.Name);
+            var to = new EmailAddress("sundayjabikem@gmail.com");
+            var message = MailHelper.CreateSingleEmail(from, to, settings.Subject, "", settings.Body);
+
+            return client.SendEmailAsync(message);
 
         }
     }
